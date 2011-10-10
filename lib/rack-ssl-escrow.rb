eb1152b5da6ec5a2ecccd858e00bd09db4fb822c
Duncan Beevers <duncan@dweebd.com>
2011-10-10 10:40:51 -0700

    Initial commit

---
+++ lib/rack-ssl-escrow.rb
@@ -0,0 +1,9 @@
+require "rack-ssl-escrow/version"
+
+module Rack
+  module Ssl
+    module Escrow
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rack-ssl-escrow/version.rb
@@ -0,0 +1,7 @@
+module Rack
+  module Ssl
+    module Escrow
+      VERSION = "0.0.1"
+    end
+  end
+end
