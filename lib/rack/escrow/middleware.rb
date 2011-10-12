d08099ad87621f63b4cc254f23eb6843bfdb450d
Duncan Beevers <duncan@dweebd.com>
2011-10-11 17:31:20 -0700

    Middleware stores response

--- lib/rack/escrow/middleware.rb
+++ lib/rack/escrow/middleware.rb
@@ -1,3 +1,4 @@
+require "uuid"
 # require "async-rack"
 
 module Rack
@@ -7,12 +8,14 @@ module Rack
       REQUEST_PATH   = 'REQUEST_PATH'
       POST           = 'POST'
       RAILS_ROUTES   = 'action_dispatch.routes'
+      LOCATION       = 'Location'
       
-      def initialize app, rails_application
+      def initialize app, rails_application, store
         puts "Initializing EscrowMiddleware"
         @rails_application = rails_application
         rails_application.config.escrow = self
-        @app = app
+        @app   = app
+        @store = store
         @recognized_escrow_segments = []
       end
 
@@ -21,12 +24,24 @@ module Rack
         [ status, header, response ]
 
         if is_escrowed? env
-          # Redirect to /escrows/work
-          status = 303 # See Other
-          response = [ 'Escrowed' ]
-        end
+          nonce = UUID.generate
 
-        [ status, header, response ]
+          response_body = []
+          response.each { |content| response_body.push(content) }
+
+          resolved_store = @store.call
+          key = "escrow:#{nonce}"
+          resolved_store.set key, [ status, header, response_body.join() ].to_json
+
+          # Set TTL on secure response
+          resolved_store.expire key, 60
+
+          # HTTP Status Code 303 - See Other
+          redirect_to = "/escrow/#{nonce}"
+          [ 303, header.merge(LOCATION => redirect_to), [ "Escrowed at #{nonce}" ] ]
+        else
+          [ status, header, response ]
+        end
       end
 
       def recognize_escrow segment
