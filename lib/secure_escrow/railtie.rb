753987a0cbfe9bed121d834da96f1bc5de26f4e4
Duncan Beevers <duncan@dweebd.com>
2011-10-13 14:00:47 -0700

    escrow_form_for rewrites url to secure domain

--- lib/secure_escrow/railtie.rb
+++ lib/secure_escrow/railtie.rb
@@ -15,6 +15,12 @@ module SecureEscrow
       DATA_ESCROW = 'data-escrow'
 
       def escrow_form_for record, options = {}, &proc
+        options[:html] ||= {}
+
+        stringy_record = String === record || Symbol === record
+        apply_form_for_options!(record, options) unless stringy_record
+
+
         form_for record, escrow_options(options), &proc
       end
 
@@ -24,8 +30,24 @@ module SecureEscrow
 
       private
       def escrow_options options
+        # Add data-escrow attribute to the form element
         html_options = options[:html] || {}
         options.merge(html: html_options.merge(DATA_ESCROW => true))
+
+        # Rewrite URL to point to secure domain
+        app = Rails.application
+        config = app.config
+
+        submission_url = controller.url_for(
+          app.routes.recognize_path(options[:url]).
+            merge(
+              host:     config.secure_domain_name,
+              protocol: config.secure_domain_protocol,
+              port:     config.secure_domain_port
+            ))
+
+        options[:url] = submission_url
+        options
       end
     end
   end
