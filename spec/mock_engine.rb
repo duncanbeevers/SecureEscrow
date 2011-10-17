36bd3694fa98f67ed13eef960f7d8c12acef9f59
Duncan Beevers <duncan@dweebd.com>
2011-10-17 12:39:42 -0700

    MockEngine can take initial configuration

--- spec/mock_engine.rb
+++ spec/mock_engine.rb
@@ -1,8 +1,30 @@
+require 'ostruct'
+
 class MockEngine
   SUCCESS = 200
 
+  def initialize extra_config = {}
+    config extra_config
+  end
+
   def call env
     [ SUCCESS, {}, [ 'nada' ] ]
+  end
+
+  def config extra_config = {}
+    @config ||= Config.new(
+      {
+        secure_domain_name:       'www.example.com',
+        secure_domain_protocol:   'https',
+        secure_domain_port:       443,
+        insecure_domain_name:     'www.example.com',
+        insecure_domain_protocol: 'http',
+        insecure_domain_port:     80
+      }.merge extra_config
+    )
+  end
+
+  class Config < OpenStruct
   end
 end
 
